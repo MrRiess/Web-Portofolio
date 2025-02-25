e27a98570acecc46cd149dc1c3ae934a952d4061
Faridz Salman Al Parissy <faridzsalman89@gmail.com>
2025-02-25 21:22:28 +0700

    update

--- assets/js/server.js
+++ assets/js/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Gedepanjang123!',
+    password: '',
     database: 'portofolio_website',
 });
 
